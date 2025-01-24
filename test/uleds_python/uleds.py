a923e458af645100e0e8991752486da74fb236e3
Craig McQueen <craig@mcqueen.au>
2025-01-24 18:17:35 +1100

    Python uleds: Create multiple uleds, and do polling

--- test/uleds_python/uleds.py
+++ test/uleds_python/uleds.py
@@ -1,24 +1,44 @@
 #!/usr/bin/python3
 
+import selectors
 import struct
 
-def run_led(name, max_brightness=255):
-    with open('/dev/uleds', 'r+b', buffering=0) as f:
+MAX_BRIGHTNESS=255
+
+led_names = ('uleds::test0', 'uleds::test1')
+
+def uleds_register(names, max_brightness=MAX_BRIGHTNESS):
+    uleds_f = []
+    for name in names:
+        f = open('/dev/uleds', 'r+b', buffering=0)
         name_struct = struct.pack('@64sI', name.encode('utf-8'), max_brightness)
-        print(type(name_struct))
         f.write(name_struct)
-        brightness_size = len(struct.pack('@I', 0))
-        previous_brightness = None
+        uleds_f.append((name, f))
+    return uleds_f
+
+def uleds_poll(uleds_f):
+    brightness_size = len(struct.pack('@I', 0))
+    sel = selectors.DefaultSelector()
+    for name, f in uleds_f:
+        sel.register(f, selectors.EVENT_READ, name)
+    try:
         while True:
-            led_brightness_data = f.read(brightness_size)
-            if led_brightness_data:
-                led_brightness, = struct.unpack('@I', led_brightness_data)
-                if led_brightness != previous_brightness:
-                    print('Brightness {}'.format(led_brightness))
-                    previous_brightness = led_brightness
+            events = sel.select()
+            for key, _mask in events:
+                name = key.data
+                led_brightness_data = key.fileobj.read(brightness_size)
+                if led_brightness_data:
+                    led_brightness, = struct.unpack('@I', led_brightness_data)
+                    print('{}: brightness {}'.format(name, led_brightness))
+    finally:
+        for name, f in uleds_f:
+            sel.unregister(f)
 
 def main():
-    run_led('uleds::test')
+    uleds_f = uleds_register(led_names)
+    uleds_poll(uleds_f)
+    for _name, f in uleds_f:
+        f.close()
 
 if __name__ == '__main__':
     main()
