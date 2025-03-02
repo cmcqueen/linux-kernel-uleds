13cee0d08149d752aad1f9dfd1b07e52e85232a9
Craig McQueen <craig@mcqueen.au>
2025-03-02 22:48:50 +1100

    Python uleds: Minor improvements to struct formats

--- test/uleds_python/uleds.py
+++ test/uleds_python/uleds.py
@@ -5,6 +5,9 @@ import struct
 
 CHAR_DEV_NAME = '/dev/uleds'
 MAX_BRIGHTNESS=255
+SETUP_FORMAT = '@64sI'
+BRIGHTNESS_FORMAT = '@I'
+BRIGHTNESS_SIZE = struct.calcsize(BRIGHTNESS_FORMAT)
 
 led_names = ('uleds::test0', 'uleds::test1')
 
@@ -12,13 +15,12 @@ def uleds_register(names, max_brightness=MAX_BRIGHTNESS):
     uleds_f = []
     for name in names:
         f = open(CHAR_DEV_NAME, 'r+b', buffering=0)
-        name_struct = struct.pack('@64sI', name.encode('utf-8'), max_brightness)
+        name_struct = struct.pack(SETUP_FORMAT, name.encode('utf-8'), max_brightness)
         f.write(name_struct)
         uleds_f.append((name, f))
     return uleds_f
 
 def uleds_poll(uleds_f):
-    brightness_size = len(struct.pack('@I', 0))
     sel = selectors.DefaultSelector()
     for name, f in uleds_f:
         sel.register(f, selectors.EVENT_READ, name)
@@ -27,9 +29,9 @@ def uleds_poll(uleds_f):
             events = sel.select()
             for key, _mask in events:
                 name = key.data
-                led_brightness_data = key.fileobj.read(brightness_size)
+                led_brightness_data = key.fileobj.read(BRIGHTNESS_SIZE)
                 if led_brightness_data:
-                    led_brightness, = struct.unpack('@I', led_brightness_data)
+                    led_brightness, = struct.unpack(BRIGHTNESS_FORMAT, led_brightness_data)
                     print('{}: brightness {}'.format(name, led_brightness))
     finally:
         for name, f in uleds_f:
